e5aa0a5d7122904c641922d508927e23c01fec9b
Hiten Jain <hitenjain146@gmail.com>
2022-02-01 03:40:01 +0530

    added icon

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,7 @@ export default function Home({ products }) {
     <div className="bg-gray-100">
       <Head>
         <title>Amazon 2.0</title>
+        <link rel="icon" href="/amazon_icon.png" />
       </Head>
       <Header />
       <main className="max-w-screen-2xl mx-auto">
